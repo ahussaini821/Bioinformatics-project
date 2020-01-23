207576b2969cd9b1b5df5594ff54f78416d1da26
ahussaini821 <55877793+ahussaini821@users.noreply.github.com>
2020-01-23 13:39:52 +0000

    Deleting some unnecessary files

--- Scraping/Names/reg_test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-ting = "Gene names\nEif2ak3 Pek Perk\nEIF2S3 EIF2G\nEIF2S2 EIF2B\nDDX3X DBX DDX3\nEIF2S1 EIF2A\nEIF2AK2 PKR PRKR\nACTN1"
-print(ting)
-gene = "EIF2S1"
-
-mo = re.findall(gene+r".*", ting)
-print(mo)
