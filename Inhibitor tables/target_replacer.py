3a9882f30ba80519074481e8c1ca2080cb5b7ebb
ahussaini821 <55877793+ahussaini821@users.noreply.github.com>
2020-01-27 23:10:57 +0000

    Finished adjusting the other inhibitor table so protein names are replaced with accession codes where available

--- Inhibitor tables/target_replacer.py
+++ Inhibitor tables/target_replacer.py
@@ -5,6 +5,14 @@ names_table = pd.read_csv("names_final.csv")
 
 count = 0
 for index, row in inhibitor_table.iterrows():
+    curr_acc_list = []
+    try:
+        other_names = row["other target"]
+        other_names = other_names.split(" ; ")
+    except:
+        continue
+
+
     match = False
     x = str(row["target"])
     try:
@@ -22,9 +30,17 @@ for index, row in inhibitor_table.iterrows():
             inhibitor_table.at[index,"target"] = row2["Accession"]
             match = True
             break
+    for i in other_names:
+        for index2, row2 in names_table.iterrows():
+            y = str(row2["Main gene name"]) + str(row2["Other gene names"]) + str(row2["Main protein name"]) + str(row2["Other protein names"])
+            other_name = i.lower()
+            if other_name in y:
+                curr_acc_list.append(row2["Accession"])
+        curr_acc = '; '.join(curr_acc_list)
+        inhibitor_table.at[index,"other target"] = curr_acc
     if match == True:
         print(count)
         continue
 
 
-inhibitor_table.to_csv("inhibitor_test.csv", index=False)
+inhibitor_table.to_csv("new_inhibitor_accession.csv", index=False)
--- Inhibitor tables/test.py
+++ Inhibitor tables/test.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("inhibitor_test.csv")
+inhibitor_table = pd.read_csv("new_kinase_inhibitor_list.csv")
 
-df.to_csv("inhibitor_kianse.csv", index=False)
+for index, row in inhibitor_table.iterrows():
+    try:
+        other_names = row["other target"]
+        other_names = other_names.split(" ; ")
+    except:
+        continue
+    for index,i in enumerate(other_names):
+        other_names[index] = i.rstrip()
+    print(other_names)
