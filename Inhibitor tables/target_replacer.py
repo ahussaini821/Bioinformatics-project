284a1162b9304a26d18a24ccf1f8a25cb044aa37
ahussaini821 <55877793+ahussaini821@users.noreply.github.com>
2020-01-27 18:24:51 +0000

    Other inhibitor table replaced protein names with accessions

--- Inhibitor tables/target_replacer.py
+++ Inhibitor tables/target_replacer.py
@@ -1,22 +1,25 @@
 import pandas as pd
 
-inhibitor_table = pd.read_csv("inhibitor_result.csv")
+inhibitor_table = pd.read_csv("new_kinase_inhibitor_list.csv")
 names_table = pd.read_csv("names_final.csv")
 
 count = 0
 for index, row in inhibitor_table.iterrows():
     match = False
-    x = str(row["Target"])
+    x = str(row["target"])
     try:
         x = x.replace("-", "")
     except:
         continue
+    x = x.lower()
     for index2, row2 in names_table.iterrows():
+        y = str(row2["Main gene name"]) + str(row2["Other gene names"]) + str(row2["Main protein name"]) + str(row2["Other protein names"])
+        y = y.lower()
 
-        if x in (str(row2["Main gene name"]) + str(row2["Other gene names"]) + str(row2["Main protein name"]) + str(row2["Other protein names"])):
+        if x in y:
             count += 1
 
-            inhibitor_table.at[index,"Target"] = row2["Accession"]
+            inhibitor_table.at[index,"target"] = row2["Accession"]
             match = True
             break
     if match == True:
